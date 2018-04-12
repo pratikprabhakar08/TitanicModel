da62c31c333c9133b625711fd020da0d45cd4259
Pratik Prabhakar <pratikprabhakar08@gmail.com>
2018-04-12 22:43:03 +0100

    Random Forest Implemenetation

--- Titanic_Test.R
+++ Titanic_Test.R
@@ -18,8 +18,8 @@ sapply(titanic.df, function (x) sum(is.na(x)))
 #Making the data variables as factors
 titanic.df$Survived <- as.factor(titanic.df$Survived)
 titanic.df$Pclass <- as.factor(titanic.df$Pclass)
-titanic.df$SibSp <- as.factor(titanic.df$SibSp)
-titanic.df$Parch <- as.factor(titanic.df$Parch)
+#titanic.df$SibSp <- as.factor(titanic.df$SibSp)
+#titanic.df$Parch <- as.factor(titanic.df$Parch)
 
 #Extract the title from the name
 extractTitle <- function(name, nop) {
@@ -60,15 +60,18 @@ titanic.df$Cabin <- NULL
 #Taking median of Age as the missing values
 titanic.df$Age[is.na(titanic.df$Age)] <- median(titanic.df$Age, na.rm=TRUE)
 
+familySize <- titanic.df$SibSp+titanic.df$Parch+1
+titanic.df$FamilySize <- as.integer(familySize)
 #Attaching Data to the R Environment
 attach(titanic.df)
 
+str(titanic.df)
 #Logistic Regression Model
-titanic.glm <- glm(Survived ~ Pclass+Sex+Age+SibSp+Parch+Embarked+Title, family=binomial(link="logit"))
+titanic.glm <- glm(Survived ~ Pclass+Sex+Age+Parch+Embarked+Title+FamilySize, family=binomial(link="logit"))
 summary(titanic.glm)
 
 #Removing Parch and Embarked as these are not significant
-titanic.glm2 <- glm(Survived ~ Pclass+Sex+Age+SibSp+Title, family=binomial(link="logit"))
+titanic.glm2 <- glm(Survived ~ Pclass+Sex+Age+Title+FamilySize:Age, family=binomial(link="logit"))
 summary(titanic.glm2)
 
 #Comparing the glm models
@@ -108,6 +111,12 @@ ggplot(titanic.df, aes(x = Title, fill = Survived)) +
 #Test dataset
 titanic_test.df <- read.csv(file="test.csv",header=TRUE, na.strings = "")
 
+#Check the total 'NA' datas
+sum(is.na(titanic_test.df))
+
+#Check the 'NA' datas according to the variables
+sapply(titanic_test.df, function (x) sum(is.na(x)))
+
 #Adding Survived variable to the test data with None value
 titanic_test.df <- data.frame('Survived' = rep("None", nrow(titanic_test.df)), titanic_test.df[,])
 
@@ -122,19 +131,35 @@ titanic_test.df$Title <- as.factor(titles)
 
 titanic_test.df$Survived <- as.factor(titanic_test.df$Survived)
 titanic_test.df$Pclass <- as.factor(titanic_test.df$Pclass)
-titanic_test.df$SibSp <- as.factor(titanic_test.df$SibSp)
-titanic_test.df$Parch <- as.factor(titanic_test.df$Parch)
-
+#titanic_test.df$SibSp <- as.factor(titanic_test.df$SibSp)
+#titanic_test.df$Parch <- as.factor(titanic_test.df$Parch)
+str(titanic_test.df)
 titanic_test.df$Age[is.na(titanic_test.df$Age)] <- median(titanic_test.df$Age, na.rm=TRUE)
+titanic_test.df$Fare[is.na(titanic_test.df$Fare)] <- median(titanic_test.df$Fare, na.rm=TRUE)
+
+familySize <- titanic_test.df$SibSp+titanic_test.df$Parch+1
+titanic_test.df$FamilySize <- as.integer(familySize)
 
 #Predicting the test data from the training model
-titanic_predict <- c('Pclass','Sex', 'Age', 'SibSp', 'Title')
+titanic_predict <- c('Pclass', 'Title','FamilySize','Fare')
 titanic_test_predict.df <- predict(titanic.glm2, titanic_test.df[,titanic_predict], type="response")
 head(titanic_test_predict.df)
 head(titanic_test.df)
 
 y_pred = ifelse(titanic_test_predict.df > 0.5, 1, 0)
-head(y_pred)
+head(y_rf_pred)
 
 output <- data.frame(cbind('PassengerId' = titanic_test.df$PassengerId, 'Survived' =y_pred))
-write.csv(output, "output.csv",row.names = FALSE)
+write.csv(output, "output6.csv",row.names = FALSE)
+
+#Random Forest Implementation
+library(randomForest)
+set.seed(1234)
+rf <- randomForest(Survived ~ Pclass+Title+FamilySize+Fare, titanic.df)
+
+predict(rf, titanic_test.df[,titanic_predict])
+rf_predict <- predict(rf, titanic_test.df[,titanic_predict])
+head(rf_predict)
+
+output <- data.frame(PassengerId = titanic_test.df$PassengerId, Survived =rf_predict)
+write.csv(output, "output13.csv",row.names = FALSE)
